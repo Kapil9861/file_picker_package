4fe9ca454086a12acff957e9ad30b3e2b564a12d
Kapil9861 <66173137+Kapil9861@users.noreply.github.com>
2025-02-17 23:11:02 +0545

    added the filepicker plugin class

---
+++ android/app/src/main/kotlin/com/example/file_picker_package/FilePickerPlugin.kt
@@ -0,0 +1,14 @@
+package com.example.file_picker_package  // Update this to match your new package name
+
+import androidx.annotation.NonNull
+import io.flutter.embedding.engine.plugins.FlutterPlugin
+
+class FilePickerPlugin : FlutterPlugin {
+    override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+        // Plugin registration logic
+    }
+
+    override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+        // Cleanup logic
+    }
+}
